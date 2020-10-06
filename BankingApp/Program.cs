84e976ba215bcd48b370cb0ca4c8ae78e6ebe8b7
Félix Étienne Labrie <fel.labrie@live.ca>
2020-10-06 13:34:01 -0400

    Add project files.

---
+++ BankingApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

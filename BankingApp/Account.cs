6d83d2a36ce6b8831d2db6a89f7cdbcce91dfb5b
Marina Melnichuk <1431488@champlaincollege.qc.ca>
2020-10-12 17:32:39 -0400

    Changes to part 2

--- BankingApp/Account.cs
+++ BankingApp/Account.cs
@@ -6,26 +6,53 @@ using System.Threading.Tasks;
 
 namespace BankingApp
 {
-    abstract class Account 
+    public abstract class Account : IAccount
     {
-        private double StartingBalance;
-        private double CurrentBalance;
-        private double TotalDeposits;
-        private double AmountofDeposits;
-        private double TotalWithdrawls;
-        private double InterestRate;
-        private double MonthServiceCharge;
-        private double AccountStatus;
+        private double startingBalance;
+        private double currentBalance;
+        private double totalDeposits;
+        private double amountofDeposits;
+        private double totalWithdrawls;
+        private double annualInterestRate;
+        private double monthServiceCharge;
+        private double accountStatus;
 
-        public Account(double CurrentBalance, double InterestRate)
+        public Account(double balance, double interest)
         {
-            CurrentBalance = this.CurrentBalance;
-            InterestRate = this.InterestRate;
+            currentBalance = balance;
+            annualInterestRate = interest;
         }
 
-        static void MakeDeposit(double Deposit)
+        public void MakeDeposit(double deposit)
         {
-            
+           currentBalance += deposit;
         }
+        
+        public void MakeWithdrawl (double withdrawl)
+        {
+           currentBalance -= withdrawl;
+        }
+        public double CalculateInterest(double yearlyInterest, double monthlyInterestRate, double monthlyInterest, double balance)
+        {
+            annualInterestRate = yearlyInterest;
+            currentBalance = balance;
+            monthlyInterestRate = (yearlyInterest / 12);
+            monthlyInterest = (balance * monthlyInterestRate);
+            balance = balance + monthlyInterest;
+            return balance;
+        }
+
+        public string CloseAndReport(double oldBalance, double newBalance, double service, double change, string str)
+        {
+            currentBalance = oldBalance;
+            monthServiceCharge = service; 
+            newBalance = oldBalance + service; 
+
+            str = "Previous Balance: " + oldBalance + "\nNew Balance: " + newBalance;
+
+            return str; 
+
+        }
+
     }
 }

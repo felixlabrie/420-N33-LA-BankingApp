222cf19b318f316106dafac67a3809ec1afa3aa3
Marina Melnichuk <1431488@champlaincollege.qc.ca>
2020-10-12 12:24:54 -0400

    Started working on Account abstract class

---
+++ BankingApp/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp
+{
+    abstract class Account 
+    {
+        private double StartingBalance;
+        private double CurrentBalance;
+        private double TotalDeposits;
+        private double AmountofDeposits;
+        private double TotalWithdrawls;
+        private double InterestRate;
+        private double MonthServiceCharge;
+        private double AccountStatus;
+
+        public Account(double CurrentBalance, double InterestRate)
+        {
+            CurrentBalance = this.CurrentBalance;
+            InterestRate = this.InterestRate;
+        }
+
+        static void MakeDeposit(double Deposit)
+        {
+            
+        }
+    }
+}
